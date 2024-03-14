27424ae4a222c04ef0e1937298d1672e402cd926
Mandeep Singh Brar <mandeep@virsainfomedia.com>
2024-03-15 00:00:09 +0530

    fix expressions

--- server/components/expressionsmanager.go
+++ server/components/expressionsmanager.go
@@ -1,11 +1,12 @@
 package components
 
 import (
+	"laatoo.io/sdk/ctx"
 	"laatoo.io/sdk/datatypes"
 	"laatoo.io/sdk/server/core"
 )
 
 type ExpressionsManager interface {
 	RegisterExpression(ctx core.ServerContext, expression core.Expression, dtype datatypes.DataType) error
-	GetExpressionValue(ctx core.RequestContext, expression core.Expression, vars core.StringMap) (interface{}, error)
+	GetExpressionValue(ctx ctx.Context, expression core.Expression, vars core.StringMap) (interface{}, error)
 }
