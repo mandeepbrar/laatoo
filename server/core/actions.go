9ce006e4c5752fa9fc0001d9bba8fada95369c3b
Mandeep Singh Brar <mandeep@virsainfomedia.com>
2024-04-21 12:10:26 +0530

    introduced more actions

--- server/core/actions.go
+++ server/core/actions.go
@@ -5,13 +5,15 @@ import "laatoo.io/sdk/config"
 type ActionType string
 
 const (
-	Transform     ActionType = "Transform"
-	Notify                   = "Notify"
-	SaveData                 = "SaveData"
-	StartWorkflow            = "StartWorkflow"
-	DeleteData               = "DeleteData"
-	UpdateDataset            = "UpdateDataset"
-	InvokeScript             = "InvokeScript"
+	Transform         ActionType = "Transform"
+	Notify                       = "Notify"
+	SaveData                     = "SaveData"
+	StartWorkflow                = "StartWorkflow"
+	DeleteData                   = "DeleteData"
+	UpdateDataset                = "UpdateDataset"
+	InvokeScript                 = "InvokeScript"
+	InvokeActivity               = "InvokeActivity"
+	ValidateArguments            = "ValidateArguments"
 )
 
 type Action interface {
