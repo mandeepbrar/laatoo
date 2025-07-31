e9598c60489b54cd7eda50f45c3b90b36a19b065
Mandeep Singh Brar <mandeep@virsainfomedia.com>
2025-07-31 16:32:20 +0530

    added notification queues

--- server/components/notifications.go
+++ server/components/notifications.go
@@ -5,4 +5,5 @@ import "laatoo.io/sdk/server/core"
 type NotificationManager interface {
 	SendNotification(ctx core.RequestContext, notification *core.Notification) error
 	Broadcast(ctx core.RequestContext, notif *core.Notification) error
+	GetQueue() string
 }
