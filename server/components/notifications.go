e943ebb37649128910e2eb4f1e00895113780923
Mandeep Singh Brar <mandeep@virsainfomedia.com>
2025-07-31 16:52:30 +0530

    changes to notifications

--- server/components/notifications.go
+++
@@ -1,9 +0,0 @@
-package components
-
-import "laatoo.io/sdk/server/core"
-
-type NotificationManager interface {
-	SendNotification(ctx core.RequestContext, notification *core.Notification) error
-	Broadcast(ctx core.RequestContext, notif *core.Notification) error
-	GetQueue() string
-}
--- server/elements/notificationmanager.go
+++ server/elements/notificationmanager.go
@@ -7,6 +7,8 @@ import (
 
 type NotificationManager interface {
 	core.ServerElement
-	components.NotificationManager
 	components.NotifiersRegistry
+	SendNotification(ctx core.RequestContext, notification *core.Notification) error
+	Broadcast(ctx core.RequestContext, notif *core.Notification) error
+	RegisterNotificationQueue(ctx core.ServerContext, notifType core.NotificationType, queue string) error
 }
